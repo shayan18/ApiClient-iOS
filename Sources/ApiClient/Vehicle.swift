43f53aeedea9de6495a0494c3a2a3ce4de6fec28
Shayan <sa@papershift.com>
2022-07-01 22:14:46 +0200

    Make model attributes accessible

--- Sources/ApiClient/Vehicle.swift
+++ Sources/ApiClient/Vehicle.swift
@@ -9,15 +9,15 @@ import Foundation
 
 // MARK: - Vehicle
 public struct Vehicle: Decodable, Equatable {
-    let batteryLevel: Int
-    let lat: Double
-    let lng: Double
-    let maxSpeed: Int
-    let vehicleType: VehicleType
-    let hasHelmetBox: Bool
+   public let batteryLevel: Int
+   public let lat: Double
+   public let lng: Double
+   public let maxSpeed: Int
+   public let vehicleType: VehicleType
+   public let hasHelmetBox: Bool
 }
 
-enum VehicleType: String, Decodable {
+public enum VehicleType: String, Decodable {
     case ebicycle = "ebicycle"
     case emoped = "emoped"
     case escooter = "escooter"
