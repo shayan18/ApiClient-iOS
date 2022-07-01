21b656ca76acfb402150b7ca50d9058720aae2d9
Shayan <sa@papershift.com>
2022-07-01 17:37:46 +0200

    Add unit tests

--- Tests/ApiClientTests/VehicleTests.swift
+++ Tests/ApiClientTests/VehicleTests.swift
@@ -1,8 +1,22 @@
-import XCTest
+// Copyright © 2021 Papershift GmbH. All rights reserved.
+
 @testable import ApiClient
+import Microya
+import XCTest
 
 final class VehicleTests: XCTestCase {
-    func testExample() throws {
+  let apiKey: String = "9ef7d5b3-21c7-4a78-a92b-91efef42cabb"
 
+  func testVehicle() throws {
+    let vehicleResponseBody =
+      try testApiProvider.performRequestAndWait(
+        on: .vehicle(apiKey: apiKey),
+        decodeBodyTo: ApiCollectionResponse<Vehicle>.self
+      )
+      .get()
+    for (index, _) in vehicleResponseBody.data.enumerated() {
+      XCTAssertNotNil(vehicleResponseBody.data[index].attributes.batteryLevel)
+      XCTAssertNotNil(vehicleResponseBody.data[index].attributes.maxSpeed)
     }
+  }
 }
